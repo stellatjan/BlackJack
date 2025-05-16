6a8c84259b1a103cfa780a281ec541c21d957036
stellatjan <stella_tjan27@milton.edu>
2025-05-16 13:49:49 -0400

    inital

---
+++ src/Cards.java
@@ -0,0 +1,7 @@
+public class Cards {
+   public String number;
+   public String suit;
+   public boolean aceValue; //i.e for ace is it 11 or 1
+   public int value;
+
+}
---
+++ src/Dealer.java
@@ -0,0 +1,8 @@
+import java.lang.reflect.Array;
+
+public class Dealer {
+    public int cardTotal;
+    public Array[]Cards;
+    public boolean isOver17;
+
+}
