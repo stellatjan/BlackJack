adc7af4b88fa151562e8bd855c0f7844eaf81451
stellatjan <stella_tjan27@milton.edu>
2025-05-17 11:00:33 -0400

    HW for Wed

--- src/PlayBlackJack.java
+++ src/PlayBlackJack.java
@@ -1,5 +1,11 @@
+import javax.smartcardio.Card;
+
 public class PlayBlackJack {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Let's play Blackjack!");
+        Player player = new Player();
+        Dealer dealer = new Dealer();
+        Cards cardDeck = new Cards();
+
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,6 @@
 public class Player {
+    public int cardTotal;
+    public boolean isHit;
+    public boolean isStand;
+    public boolean isSplit;
 }
